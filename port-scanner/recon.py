4dc64ca8cd63643cfbd148c0d9479b993ae75024
Manish Burdak <manishburdak4545@gmail.com>
2026-04-27 09:20:43 +0530

    Update recon.py

--- port-scanner/recon.py
+++ port-scanner/recon.py
@@ -1,1 +1,28 @@
+import socket
 
+target = input("Enter Target IP: ")
+
+ports = [21, 22, 80, 443, 8080]
+
+print(f"\nScanning {target}...\n")
+
+for port in ports:
+    try:
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect((target, port))
+
+        print(f"[+] Port {port} OPEN")
+
+        try:
+            banner = s.recv(1024).decode().strip()
+            print(f"    ↳ Service Info: {banner}")
+        except:
+            print("    ↳ No banner received")
+
+        s.close()
+
+    except:
+        pass
+
+print("\nScan Complete ✅")
